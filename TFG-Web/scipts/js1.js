84b84b457fcc52e41467b296e13941a365e36c23
alu0100890709 <32129884+alu0100890709@users.noreply.github.com>
2019-06-22 20:40:37 +0100

    Progreso medio

---
+++ TFG-Web/scipts/js1.js
@@ -0,0 +1,60 @@
+function cambiar(esto)
+{
+	vista=document.getElementById(esto).style.display;
+	if (vista=='none')
+		vista='block';
+	else
+		vista='none';
+
+	document.getElementById(esto).style.display = vista;
+}
+
+function enviar(pagina){
+	document.nombreDelFormulario.action = pagina;
+	document.nombreDelFormulario.submit();
+	
+}
+
+function mover(pagina){
+	location.href=pagina;
+	
+}
+
+function navbarInf(){
+	var elemento = document.getElementById("Info");
+    if (elemento.className == "active") {
+	  elemento.className = "desactive";
+
+    }else {
+	  elemento.className = "active";
+	  cambiar('Información');
+	  if(document.getElementById("Requirements").className == "active"){ navbarReq(); cambiar('Requisitos');}
+	  if(document.getElementById("Contact").className == "active"){ navbarCont(); cambiar('Contactos');}
+    }
+}
+
+function navbarReq(){
+	var elemento = document.getElementById("Requirements");
+    if (elemento.className == "active") {
+	  elemento.className = "desactive";
+	  
+    }else {
+	  elemento.className = "active";
+	  cambiar('Requisitos');
+	  if(document.getElementById("Info").className == "active"){ navbarInf();  cambiar('Información');}
+	  if(document.getElementById("Contact").className == "active"){ navbarCont(); cambiar('Contactos');}
+	  
+    }
+}
+
+function navbarCont(){
+	var elemento = document.getElementById("Contact");
+    if (elemento.className == "active") {
+      elemento.className = "desactive";
+    }else {
+	  elemento.className = "active";
+	  cambiar('Contactos');
+	  if(document.getElementById("Info").className == "active"){ navbarInf();  cambiar('Información');}
+	  if(document.getElementById("Requirements").className == "active"){ navbarReq(); cambiar('Requisitos');}
+    }
+}
